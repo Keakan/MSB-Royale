0ff0e24b7040f12b177c83e54d6b054ee46f6713
JonHolt <jonathanholt92@gmail.com>
2016-04-13 09:37:49 -0600

    better feeling controls

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -4,12 +4,21 @@ using System.Collections;
 public class Control : MonoBehaviour {
     public string playerNum = "";
     public float moveSpeed = 5.0f;
+    public float jumpPower = 20f;
+    public float fastFall = 10f;
+    public bool jumping = false;
+    bool facingRight = true;
 
-    private bool jumping = false;
     private Rigidbody2D rb;
+    private Animator anim;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("Ground", true);
+        anim.SetFloat("Speed", 0f);
+        anim.SetBool("Crouch", false);
     }
 
 	void FixedUpdate()
@@ -19,7 +28,8 @@ public class Control : MonoBehaviour {
             if (!jumping)
             {
                 jumping = true;
-                rb.velocity = new Vector2(rb.velocity.x, 10);
+                rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+                anim.SetBool("Ground", false);
             }
         }
         if (Input.GetButton("B_" + playerNum))
@@ -39,27 +49,45 @@ public class Control : MonoBehaviour {
         if ( x > 0)
         {
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            if (!facingRight)
+            {
+                Flip();
+            }
+            anim.SetFloat("Speed", 1);
         }
         if (x < 0)
         {
             rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+            if (facingRight)
+            {
+                Flip();
+            }
+            anim.SetFloat("Speed", 1);
         }
         if(x == 0)
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
+            anim.SetFloat("Speed", 0);
         }
-        if (y > 0)
+        if (y > 0.5f /*&& jumping*/)
         {
-            //Debug.Log("Down");
+            rb.velocity = new Vector2(rb.velocity.x, -fastFall);
+            
         }
         if (y < 0)
         {
-            //Debug.Log("Up");
+            //up
         }
     }
 
-    void OnCollisionEnter2D(Collider2D other)
+    void Flip()
     {
+        // Switch the way the player is labelled as facing
+        facingRight = !facingRight;
 
+        // Multiply the player's x local scale by -1
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
---
+++ Assets/Scripts/doneJumping.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doneJumping : MonoBehaviour {
+
+    public Control notify;
+    public Animator anim;
+	
+	void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Floor")
+        {
+            notify.jumping = false;
+            anim.SetBool("Ground", true);
+        }
+    }
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            notify.jumping = false;
+            anim.SetBool("Ground", true);
+        }
+    }
+}
