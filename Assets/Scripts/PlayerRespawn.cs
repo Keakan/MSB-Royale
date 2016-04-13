1fb2faa9c64dcfe4059ed9d89b918c6adcb09027
WebsterBagley <splatmanrocks@gmail.com>
2016-04-13 10:31:30 -0600

    Merge branch 'master' into Respawn_Added
    # Conflicts:
    #	Assets/Scenes/Level 1.unity
    #	ProjectSettings/GraphicsSettings.asset
    #	ProjectSettings/ProjectSettings.asset
    #	ProjectSettings/ProjectVersion.txt
    #	ProjectSettings/TagManager.asset

---
+++ Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRespawn : MonoBehaviour {
+
+    public GameObject RespawnPoint;
+    public int PlayerHealth;
+    public int PlayerNumber;
+    private void Start()
+    {
+        
+    }
+    private void Kill()
+    {
+        string KillMessage = "Player " + PlayerNumber + " Died";
+        string DeadMessage = "Player " + PlayerNumber + " Has No More Lives!";
+
+        if (PlayerHealth > 1)
+        {
+            this.transform.position = RespawnPoint.transform.position;
+            PlayerHealth -= 1;
+            Debug.Log( KillMessage);
+            
+        } 
+        else
+        {
+            Debug.Log(DeadMessage);
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "KillZone")
+        {
+            Kill();
+        }
+        
+    }
+}
