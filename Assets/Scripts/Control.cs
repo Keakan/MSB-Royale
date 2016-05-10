658282b43e0c0a5871ec3cfc29be8023ba03de59
WebsterBagley <splatmanrocks@gmail.com>
2016-05-10 14:48:50 -0600

    icy ice level
    its so very icy, yes so icy indeeed

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -48,7 +48,7 @@ public class Control : MonoBehaviour {
         }
         if (Input.GetButton("B_" + playerNum))
         {
-            //Debug.Log("B Pressed");
+            //Debug.Log("B Pressed by Player " + playerNum);
         }
         //push attack, removed because it sucks
         /*if (Input.GetButton("X_" + playerNum))
